1c4c774f0edd8301d16fbfdfa4efd6f6bfbf90c5
bellisaa <131073380+bellisaa@users.noreply.github.com>
2023-06-30 09:14:21 -0400

    Add files via upload

--- js rider/rider.js
+++ js rider/rider.js
@@ -1,7 +1,7 @@
 
 function krRevice() {var element = document.body;element.classList.toggle("krRevice");}
 
-function krZeroOne() {var element = document.body;element.classList.toggle("krZeroOne");}
+function krZeroOne() {var element = document.body;element.classList.toggle("krZeroOne")}
 
 function f5(){window.location.reload(true);} 
 
